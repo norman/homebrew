e44caca24fe18618a639498c86e1bdcbcf4898d0
Peter Aronoff <telemachus@arpinum.org>
2010-08-16 12:52:20 -0400

    Update Vim to 7.3
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/vim.rb
+++ Library/Formula/vim.rb
@@ -1,10 +1,10 @@
 require 'formula'
 
 class Vim <Formula
-  url 'http://vim.svn.sourceforge.net/svnroot/vim/branches/vim7.2/', :revision => '1889'
-  version '7.2.446'
-  head 'http://vim.svn.sourceforge.net/svnroot/vim/trunk/'
+  url 'ftp://ftp.vim.org/pub/vim/unix/vim-7.3.tar.bz2'
   homepage 'http://www.vim.org/'
+  md5 '5b9510a17074e2b37d8bb38ae09edbf2'
+  head 'http://vim.svn.sourceforge.net/svnroot/vim/trunk/'
 
   def install
     system "./configure", "--prefix=#{prefix}",
